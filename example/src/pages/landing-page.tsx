71ad0b16c22c84158b68a7b796ab8820f6f13763
Quick Vite <199968227+quick-vite@users.noreply.github.com>
2025-02-21 23:58:32 +0100

    fix: Fixed url routing issue

--- example/src/pages/landing-page.tsx
+++ example/src/pages/landing-page.tsx
@@ -1,9 +1,10 @@
 import { A } from "@solidjs/router";
 import { Component } from "solid-js";
+import { routeBase } from '@quick-vite/gh-pages-spa/solidjs'
 
 export const LandingPage: Component = () => <>
 	<p>TODO description on how this all works.</p>
 	<p>Perhaps just import the markdown.</p>
-	<p><a href={`${import.meta.env.routeBase}/example/1/`}>Example 1</a></p>
+	<p><a href={`${routeBase}/example/1/`}>Example 1</a></p>
 	<p><A href="/example/2/">Example 2</A></p>
 </>
